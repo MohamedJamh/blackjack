0dfbc1c5fae07005536a1789f9f79630b6b64169
MohamedJamh <jam7.mohamed@gmail.com>
2023-09-25 13:34:42 +0100

    Implementing Gambler interactions logic , starting a game , round , working on spreading cards method

--- src/Controller/DeckController.java
+++ src/Controller/DeckController.java
@@ -15,6 +15,8 @@ public class DeckController {
     public static Gambler gambler;
     public static int[][] cardsBank;
     public static Cards cards;
+
+    public static int[] bets = new int[]{1, 5, 25, 50, 100, 500,1000};
     private static final Scanner myScanner = new Scanner(System.in);
 
     public static void opening() {
@@ -32,9 +34,48 @@ public class DeckController {
         int[][] cards = CardsService.createCardsList(1, CardSymbol.DIAMOND);
         cards = DeckService.shiftCards(cards);
         DeckService.drawPlayingCards(cards);
-        startRound();
+        do {
+            startRound();
+            if(Gambler.getBudget() < 5){
+                System.out.println("Sorry Out Of Budget");
+                break;
+            }
+            else{
+                System.out.println("Would you like another round :\n");
+                System.out.println("Y for yes :\n");
+                System.out.println("Press key for no :\n");
+                if(myScanner.nextLine().equalsIgnoreCase("Y")) startRound();
+            }
+        }while (true);
     }
-    public static void startRound(){}
+    public static void startRound(){
+        System.out.printf("------------------ ROUND ------------------ budget: %f\n",Gambler.getBudget());
+        int betChoice;
+        do {
+            System.out.println("Available bets :");
+            for (int i = 0; i < bets.length; i++) {
+                if( bets[i] <= Gambler.getBudget() ) System.out.printf("%d - %d Dollars .\n", i+1, bets[i]);
+            }
+            System.out.println("99 - All In ");
+            try {
+                System.out.println("Please Enter Your Bet :");
+                betChoice = Integer.parseInt(myScanner.nextLine());
+                if(betChoice - 1 < bets.length){
+                    Gambler.setBet(bets[betChoice - 1]);
+                    break;
+                } else if (betChoice == 99) {
+                    Gambler.setBet(Gambler.getBudget());
+                    break;
+                }
+            }catch (Exception ignored){}
+        }while (true);
+    }
+
+    public static void spreadCards(){
+        for (int i = 1; i <= 4 ; i++) {
+
+        }
+    }
     public static void endGame(){
         //TODO:This method end the blackjack game show remaining balance
     }
--- src/Domain/Gambler.java
+++ src/Domain/Gambler.java
@@ -2,11 +2,17 @@ package Domain;
 
 public class Gambler extends Player {
     private static double budget;
-
+    private static double bet;
+    public static double getBet() {
+        return bet;
+    }
+    public static void setBet(double bet) {
+        Gambler.bet = bet;
+        Gambler.budget -= bet;
+    }
     public static double getBudget() {
         return budget;
     }
-
     public static void setBudget(double budget) {
         Gambler.budget = Math.floor(budget);
     }
